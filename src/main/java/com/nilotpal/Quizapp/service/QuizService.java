565b5b84faa0808469a9ef2598d2cd8b0069f87f
Neel901 <84657447+Neel901@users.noreply.github.com>
2024-08-31 06:55:00 +0530

    Add files via upload

--- src/main/java/com/nilotpal/Quizapp/service/QuizService.java
+++ src/main/java/com/nilotpal/Quizapp/service/QuizService.java
@@ -2,14 +2,18 @@ package com.nilotpal.Quizapp.service;
 
 import com.nilotpal.Quizapp.Dao.QuestionDao;
 import com.nilotpal.Quizapp.Dao.QuizDao;
+import com.nilotpal.Quizapp.model.QuestionWrapper;
 import com.nilotpal.Quizapp.model.Questions;
 import com.nilotpal.Quizapp.model.Quiz;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -31,5 +35,32 @@ public class QuizService {
 
         return new ResponseEntity<>("success", HttpStatus.CREATED);
 
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Questions> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsForUser = new ArrayList<>();
+        for(Questions q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsForUser.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUser, HttpStatus.OK);
+
+    }
+
+    public ResponseEntity<Integer> calculateResult(Integer id, List<Response> responses) {
+        Quiz quiz = quizDao.findById(id).get();
+        List<Question> questions = quiz.getQuestions();
+        int right = 0;
+        int i = 0;
+        for(Response response : responses){
+            if(response.getResponse().equals(questions.get(i).getRightAnswer()))
+                right++;
+
+            i++;
+        }
+        return new ResponseEntity<>(right, HttpStatus.OK);
     }
 }
