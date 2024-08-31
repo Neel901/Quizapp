85d391ae8c4faa3098bcecb322465276bf37650d
Neel901 <84657447+Neel901@users.noreply.github.com>
2024-08-31 06:54:09 +0530

    Add files via upload

---
+++ src/main/java/com/nilotpal/Quizapp/model/Response.java
@@ -0,0 +1,10 @@
+package com.nilotpal.Quizapp.model;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
